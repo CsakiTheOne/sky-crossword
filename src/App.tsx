7a6342fcd4a63a4d486f4ba031b7cab7cb72bcc8
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2024-01-02 15:58:21 +0100

    Original puzzle added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CrosswordDisplay from './components/CrosswordDisplay/CrosswordDisplay';
 import Container from './components/Container';
+import originalPuzzleImage from './media/SKY_COTL_CROSSWORD_BY_RAJ_BADIANI.jpg';
 
 function App() {
     return (
@@ -13,6 +14,23 @@ function App() {
                 </p>
             </Container>
             <CrosswordDisplay />
+            <Container>
+                <h2>Get the original puzzle</h2>
+                <p>
+                    If you want to print it or solve it in an image editor, you can download the original puzzle here:
+                </p>
+                <img
+                    src={originalPuzzleImage}
+                    alt=""
+                    style={{
+                        maxWidth: '50%',
+                        height: 'auto',
+                    }}
+                />
+                <p>
+                    <a href={originalPuzzleImage} download>Direct download</a>
+                </p>
+            </Container>
         </div>
     );
 }
